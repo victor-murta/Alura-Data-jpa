5fc90c3e8a4cf3d4c1c79f52f811543077e28392
victor-murta <vmurtag16@gmail.com>
2021-07-27 18:48:31 -0300

    Java

---
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // executar os comandos dessa classe
+public class SpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataApplication.class, args); //rodar a aplicação
+	}
+
+}
